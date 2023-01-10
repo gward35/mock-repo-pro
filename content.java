17fd1e7336fbd29449fcc72b9950b8b7214b2bf2
Gretchen <gretchen.ward@protenus.com>
2023-01-10 15:01:53 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -12,6 +12,6 @@ public class Cipuvj {
         System.out.println("ekuvp");
         System.out.println("ahmae");
         System.out.println("typki");
-        System.out.println("wxaqc");
+        System.out.println("scflr");
     }
 }
